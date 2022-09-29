2f67c1bacaf85a89f870a0e2250b778457c9879e
Colton Rennie <colton.rennie@hotmail.com>
2022-09-28 23:18:42 -0300

    migrate and added urls

--- dogcollector/settings.py
+++ dogcollector/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'dogcollector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dogcollector',
+        'USER': 'postgres',
+        'PASSWORD': 'password12',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+
     }
 }
 
