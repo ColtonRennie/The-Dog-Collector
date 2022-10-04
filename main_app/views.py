f325a845da915b8751ca4ecf5067ef056856c0bd
Colton Rennie <colton.rennie@hotmail.com>
2022-10-04 15:00:05 -0300

    added the database and the detail page

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Dog
 
-class Dog:  
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-dogs = [
-  Dog('Norah', 'Belgian Tervuren', 'She is a long haired mixed tans and black that has a lot of energy', 8),
-  Dog('Youki', 'Mixed', 'He is starting to get lazy in his old age, but is always up for a fight', 9),
-  Dog('Sadie', 'Mixed', 'She is the youngest of the pack and with lots of spunk and personality', 3)
-]
 
 def home(request):
   return render(request,'base.html')
@@ -21,5 +10,9 @@ def about(request):
   return render(request, 'about.html')
 
 def dogs_index(request):
+  dogs = Dog.objects.all()
   return render(request, 'dogs/index.html', { 'dogs': dogs })
 
+def dogs_detail(request,dog_id):
+  dog = Dog.objects.get(id=dog_id)
+  return render(request, 'dogs/detail.html', {'dog' :dog})
