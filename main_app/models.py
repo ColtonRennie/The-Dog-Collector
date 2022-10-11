0f7a2d2ea51e5c3f62daa488c50630eb65a86e05
Colton Rennie <colton.rennie@hotmail.com>
2022-10-10 22:44:36 -0300

    Added one to many relationship with feedings to dogs

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,13 @@
+from email.policy import default
+from secrets import choice
 from django.db import models
 from django.urls import reverse
+
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
 
 class Dog(models.Model):
   name = models.CharField(max_length=100)
@@ -13,3 +21,16 @@ class Dog(models.Model):
 
   def get_absolute_url(self):
     return reverse('detail', kwargs={'dog_id': self.id})
+
+
+class Feeding(models.Model):
+  date = models.DateField('Feeding Date')
+  meal = models.CharField(max_length=1, choices=MEALS, default=MEALS[0][0])
+
+  dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+
+  class Meta:
+    ordering = ['-date']  
