a1364f7cfd3a25418b191ff2a6bb67c333c2c9e8
Colton Rennie <colton.rennie@hotmail.com>
2022-10-15 11:09:18 -0300

    update toy not migrated

--- main_app/models.py
+++ main_app/models.py
@@ -9,11 +9,16 @@ MEALS = (
   ('D', 'Dinner')
 )
 
+class Toy(models.Model):
+  name = models.CharField(max_length = 50)
+  color = models.CharField(max_length = 20)
+
 class Dog(models.Model):
   name = models.CharField(max_length=100)
   breed = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   age = models.IntegerField()
+  toys = models.ManyToManyField(Toy)
 
   def __str__(self):
     return self.name
