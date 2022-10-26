0d18c094ee23714423b00d1346c2b6c8f53268d7
Colton Rennie <colton.rennie@hotmail.com>
2022-10-25 21:19:20 -0300

    Config deployment

--- dogcollector/settings.py
+++ dogcollector/settings.py
@@ -13,10 +13,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import  environ
 
+import django_heroku
+
+django_heroku.settings(locals())
 
 env = environ.Env()
 environ.Env.read_env()
 
+WSGI_APPLICATION = 'dogcollector.wsgi.application'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
